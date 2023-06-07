7a10added53b430b4bd03e498d34f7695c939c65
zs0000 <zachsanchez00@gmail.com>
2023-06-07 11:54:17 -0700

    Basic setup for animal uploads (quick upload)

---
+++ src/components/DashActionBar/DashActionBar.js
@@ -0,0 +1,14 @@
+import AnimalFormModal from "../AnimalFormModal/AnimalFormModal"
+import s from "./DashActionBar.module.css"
+
+export default function DashActionBar() {
+  return (
+    <div className={s.container}>
+        <div className={s.actionscontainer}>
+            <div className={s.actioncontainer}>
+                <AnimalFormModal
+            </div>
+        </div>
+    </div>
+  )
+}
